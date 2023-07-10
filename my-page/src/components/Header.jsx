87b49cb57ab46effcc010a5dbf674cd4b6ecca41
shubh1909 <79655639+shubh1909@users.noreply.github.com>
2023-07-10 08:25:43 +0000

    first commit

---
+++ my-page/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header(){
+    return(
+        <>
+        <div className="header-section">
+            <div className="header-content">
+                <p className="inputs">Home</p>
+                <p className="inputs">Experiences</p>
+                <p className="inputs">Projects</p>
+                <p className="inputs">Intresting Things</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Header;
